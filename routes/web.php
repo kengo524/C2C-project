7a7a3d94e06d650741fd0dda743747c01ea3e2ff
kengo524 <kengo.konishi@tokyo.supersoftware.co.jp>
2022-05-19 11:55:15 +0900

    ルート調整

--- routes/web.php
+++ routes/web.php
@@ -22,8 +22,8 @@ use App\Http\Controllers\ItemController;
 // ホーム画面
 Route::get('/', [HomeController::class, 'index'])->name('homepage');
 // 商品
-Route::get('/item', [HomeController::class, 'index'])->name('homepage');
-Route::get('/item/{id}', [ItemController::class, 'itemDetail'])->name('item_detail');
+// Route::get('/item', [HomeController::class, 'index'])->name('homepage');
+// Route::get('/item/{id}', [ItemController::class, 'itemDetail'])->name('item_detail');
 
 
 Route::get('/dashboard', function () {
@@ -34,12 +34,14 @@ require __DIR__.'/auth.php';
 
 //新規出品登録
 Route::prefix('item')->group(function (){
-    Route::get('/create', 'App\Http\Controllers\ItemController@showCreateForm')->name('item.create'); //フォーム取得
-    Route::post('/confirm', 'App\Http\Controllers\ItemController@confirm')->name('item.confirm'); //確認画面取得
-    Route::post('/create', 'App\Http\Controllers\ItemController@create'); //保存処理実行
-    Route::get('/complete', 'App\Http\Controllers\ItemController@complete')->name('item.complete'); //保存処理完了画面
-    Route::get('/edit', 'App\Http\Controllers\ItemController@edit')->name('item.edit');
-    Route::get('/delete', 'App\Http\Controllers\ItemController@edit')->name('item.delete');
+    Route::get('/', [HomeController::class, 'index'])->name('homepage');
+    Route::get('/create', [ItemController::class, 'showCreateForm'])->name('item.create'); //フォーム取得
+    Route::post('/confirm', [ItemController::class, 'confirm'])->name('item.confirm'); //確認画面取得
+    Route::post('/create', [ItemController::class, 'create']); //保存処理実行
+    Route::get('/complete', [ItemController::class, 'complete'])->name('item.complete'); //保存処理完了画面
+    Route::get('/delete', [ItemController::class, 'edit'])->name('item.edit');
+    Route::get('/delete', [ItemController::class, 'delete'])->name('item.delete');
+    Route::get('/{id}', [ItemController::class, 'itemDetail'])->name('item_detail');
 });
 
 //ユーザのルート
