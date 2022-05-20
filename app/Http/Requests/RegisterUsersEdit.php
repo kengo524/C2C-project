1f150f6ba356802acbf33614913b40376b4097d8
kaho211 <kaho.fujii@tokyo.supersoftware.co.jp>
2022-05-20 17:41:38 +0900

    'ユーザ情報変更機能のバリデーション日本語化'

--- app/Http/Requests/RegisterUsersEdit.php
+++ app/Http/Requests/RegisterUsersEdit.php
@@ -33,11 +33,40 @@ class RegisterUsersEdit extends FormRequest
             //Password
             'password' => 'required|string|min:8',
             //PhoneNumber（ハイフンなし）
-            'phone_number' => 'required|string|max:11|min:10',
+            'phone_number' => 'required|integer|max:11|min:10',
             //PostalCode（ハイフンなし）
-            'postal_code' => 'required|string|digits:7',
+            'postal_code' => 'required|integer|digits:7',
             //Address
             'address' => 'required|string',
+        ];
+    }
+    //日本語に変換
+    public function attributes()
+    {
+        return [
+            //NickName
+            'nick_name' => 'ニックネーム',
+            //Email
+            'email' => 'メールアドレス',
+            //Password
+            'password' => 'パスワード',
+            //PhoneNumber（ハイフンなし）
+            'phone_number' => '電話番号',
+            //PostalCode（ハイフンなし）
+            'postal_code' => '郵便番号',
+            //Address
+            'address' => '住所',
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'email.unique' => 'この:attribute は既に使用しています。',
+            'password.min' => ':attribute は８文字以上で設定してください。',
+            'phone_number.min' => ':attribute は１０文字以上１１文字以下で設定してください。',
+            'phone_number.max' => ':attribute は１０文字以上１１文字以下で設定してください。',
+            'postal_code.digits' => '正しい:attributeを入力してください。',
         ];
     }
 }
