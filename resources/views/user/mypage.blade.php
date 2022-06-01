4604264253be9baec30500b69278dd6a04fa165a
kaho211 <kaho.fujii@tokyo.supersoftware.co.jp>
2022-06-02 00:56:09 +0900

    'マイページデザイン変更'

--- resources/views/user/mypage.blade.php
+++ resources/views/user/mypage.blade.php
@@ -3,17 +3,43 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>mypage</title>
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>C2C App</title>
+        <link rel="stylesheet" href="/css/styles.css">
     </head>
     <body>
-      <div>ここはマイページ</div>
-      <a href="{{ route('homepage') }}">トップページへ</a>
-      <a href="{{ route('item.create') }}">新規出品</a>
-      <a href="{{ route('orders.index') }}">購入履歴</a>
-      <a href="{{ route('listing') }}">出品履歴</a>
-      <a href="{{ route('listing-sold.index') }}">成約済み商品一覧</a>
-      <a href="{{ route('cashpayment.new') }}">出金</a>
-      <a href="{{ route('register.edit', ['id'=>$user->id]) }}">ユーザー情報変更</a>
+        <header>
+            <nav class="my-navbar">
+              <a class="my-navbar-brand" href="/">C2C App</a>
+            </nav>
+          </header>
+          <div class="container">
+            <div class="row">
+              <div class="col-12">
+                <h5>マイページ</h5>
+                <form method="GET" action="{{ route('homepage') }}">
+                <button type="submit">トップページへ</button>
+                </form><br>
+                <form method="GET" action="{{ route('item.create') }}">
+                    <button type="submit">新規出品</button>
+                </form><br>
+                <form method="GET" action="{{ route('orders.index') }}">
+                    <button type="submit">購入履歴</button>
+                </form><br>
+                <form method="GET" action="{{ route('listing') }}">
+                    <button type="submit">出品履歴</button>
+                </form><br>
+                <form method="GET" action="{{ route('listing-sold.index') }}">
+                    <button type="submit">成約済み商品一覧</button>
+                </form><br>
+                <form method="GET" action="{{ route('cashpayment.new') }}">
+                    <button type="submit">出金</button>
+                </form><br>
+                <form method="GET" action="{{ route('register.edit', ['id'=>$user->id]) }}">
+                    <button type="submit">ユーザ情報変更</button>
+                </form>
+              </div>
+            </div>
+        </div>
     </body>
 </html>
