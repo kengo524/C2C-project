ad21a2ad75c0396570331bbdcd14155fad7cd628
kaho211 <kaho.fujii@tokyo.supersoftware.co.jp>
2022-06-02 00:16:54 +0900

    '購入履歴編集画面デザイン変更'

--- resources/views/order/complete.blade.php
+++ resources/views/order/complete.blade.php
@@ -1,14 +1,33 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="ja">
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>complete_orders</title>
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>C2C App</title>
+        <link rel="stylesheet" href="/css/styles.css">
     </head>
     <body>
-      <p>出品者に商品到着の旨を連絡しました。</p>
-      <p>またのご利用お待ちしております！</p>
-      <a href="{{ route('mypage') }}">マイページへ</a>
-    </body>
-</html>
+        <header>
+            <nav class="my-navbar">
+                <a class="my-navbar-brand" href="/">C2C App</a>
+            </nav>
+        </header>
+        <main>
+            <div class="container">
+              <div class="row">
+                <div class="col col-md-offset-3 col-md-6">
+                  <nav class="panel panel-default">
+                    <div class="panel-heading">完了</div>
+                    <div class="panel-body">
+                        <p>出品者に商品到着の旨を連絡しました。</p>
+                        <p>またのご利用お待ちしております！</p>
+                    </div>
+                    <a href="{{ route('mypage') }}">マイページへ</a>
+                  </nav>
+                </div>
+              </div>
+            </div>
+          </main>
+        </body>
+        </html>
